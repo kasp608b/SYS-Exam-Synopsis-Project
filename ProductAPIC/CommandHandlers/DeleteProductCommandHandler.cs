6c48e4f0d8028c0ef6a07f892c0138cea1564a5a
scupak <antonibujko@hotmail.com>
2023-05-21 23:13:56 +0200

    Update DeleteProductCommandHandler.cs

--- ProductAPIC/CommandHandlers/DeleteProductCommandHandler.cs
+++ ProductAPIC/CommandHandlers/DeleteProductCommandHandler.cs
@@ -1,6 +1,38 @@
-﻿namespace ProductAPIC.CommandHandlers
+﻿using Common.EventStoreCQRS;
+using EventStore.Client;
+using ProductAPIC.Commands;
+using SharedModels.EventStoreCQRS;
+using SharedModels.ProductAPICommon.Events;
+
+namespace ProductAPIC.CommandHandlers
 {
-    public class DeleteProductCommandHandler
+    public class DeleteProductCommandHandler : ICommandHandler<DeleteProduct>
     {
+
+        private readonly EventStoreClient _eventStore;
+
+        private readonly EventSerializer _eventSerializer;
+
+        private readonly CancellationToken _cancellationToken;
+
+        public DeleteProductCommandHandler(EventStoreClient eventStore, EventSerializer eventSerializer)
+        {
+            _eventStore = eventStore;
+            _eventSerializer = eventSerializer;
+            _cancellationToken = new CancellationToken();
+        }
+
+        public async Task HandleAsync(DeleteProduct command)
+        {
+            var @event = new ProductDeleted
+            {
+                Id = command.Id,
+                DeletedAt = DateTime.UtcNow
+            };
+
+            await _eventStore.Append(@event, "Product", _eventSerializer, _cancellationToken);
+
+
+        }
     }
 }
