646a63cd13d274933725a506d39902906bae0239
scupak <antonibujko@hotmail.com>
2023-05-22 13:37:57 +0200

    Create IEventHandler.cs

---
+++ SharedModels/EventStoreCQRS/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedModels.EventStoreCQRS
+{
+    public interface IEventHandler<TEvent> where TEvent : IEvent
+    {
+        Task HandleAsync(TEvent @event);
+    }
+    
+        
+    
+}
