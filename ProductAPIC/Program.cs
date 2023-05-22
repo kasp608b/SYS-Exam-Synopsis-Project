1a22e598986dd18e28a817aefc92d4fb226e4603
scupak <antonibujko@hotmail.com>
2023-05-22 19:34:10 +0200

    Added CommandHandlers to dependency injection

--- ProductAPIC/Program.cs
+++ ProductAPIC/Program.cs
@@ -1,6 +1,37 @@
+using Common.EventStoreCQRS;
+using EventStore.Client;
+using ProductAPIC.Command;
+using ProductAPIC.CommandHandlers;
+using ProductAPIC.Commands;
+using SharedModels.EventStoreCQRS;
+using SharedModels.ProductAPICommon.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Connect to EventStoreDB.
+string gRpcConnectionString = "esdb://producteventstore.db:2113?tls=false";
+
 // Add services to the container.
+builder.Services.AddEventStoreClient(gRpcConnectionString);
+
+builder.Services.AddScoped<EventSerializer>();
+
+builder.Services.AddScoped<EventDeserializer>();
+
+builder.Services.AddScoped<ICommandHandler<AddItemsToStock>, AddItemsToStockCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<RemoveItemsFromStock>, RemoveItemsFromStockCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<ChangeProductCategory>, ChangeProductCategoryCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<ChangeProductName>, ChangeProductNameCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<ChangeProductPrice>, ChangeProductPriceCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<CreateProduct>, CreateProductCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<DecreaseReservedItems>, DecreaseReservedItemsCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<IncreaseReservedItems>, IncreaseReservedItemsCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<DeleteProduct>, DeleteProductCommandHandler>();
+builder.Services.AddScoped<ICommandHandler<ShipProduct>, ShipProductCommandHandler>();
+
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
