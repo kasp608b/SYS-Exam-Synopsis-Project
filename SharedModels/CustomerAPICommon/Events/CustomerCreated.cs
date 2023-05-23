f215d9d9957a45e022a1df4de1c34877ef2c0d0c
kasp608b <54952896+kasp608b@users.noreply.github.com>
2023-05-23 17:46:18 +0200

    Added the events for customer

---
+++ SharedModels/CustomerAPICommon/Events/CustomerCreated.cs
@@ -0,0 +1,25 @@
+﻿using SharedModels.EventStoreCQRS;
+
+namespace SharedModels.CustomerAPICommon.Events
+{
+    public class CustomerCreated : IEvent
+    {
+        public Guid Id { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string BillingAddress { get; set; }
+
+        public string ShippingAddress { get; set; }
+
+        public bool CreditStanding { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ SharedModels/CustomerAPICommon/Events/CustomerCreditStandingChanged.cs
@@ -0,0 +1,13 @@
+﻿using SharedModels.EventStoreCQRS;
+
+namespace SharedModels.CustomerAPICommon.Events
+{
+    public class CustomerCreditStandingChanged : IEvent
+    {
+        public Guid Id { get; set; }
+
+        public bool CreditStanding { get; set; }
+
+        public DateTime CreditStandingChangedAt { get; set; }
+    }
+}
